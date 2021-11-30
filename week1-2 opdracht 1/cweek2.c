bc0b766ab1c3766b2a4fcf54f0853c95d6d6d118
geerteethawaiipizza <71379796+geerteethawaiipizza@users.noreply.github.com>
2021-11-30 09:42:49 +0100

    oeps
    oeps

--- week1-2 opdracht 1/cweek2.c
+++ week1-2 opdracht 1/cweek2.c
@@ -3,31 +3,31 @@
 
 int main()
 {
-    int a, b;
+	int a, b;
 	char star = '*';
 	char dash = '-';
-    printf("Kies optie(negatief getal,positief getal, of 2):");
-    scanf("%d", &a);
-	if (a == 2)
+	printf("Kies optie(negatief getal,positief getal, of 2):");
+	scanf("%d", &a);
+	if (a = 2)
 	{
-		for (b= 0 ; b < 10; b++ )
+		for (b = 0; b < 10; b++)
 		{
-			print_function ( star );
-			print_function( dash );
+			print_function(star);
+			print_function(dash);
 			printf("\n");
 		}
 	}
 
 	else if (a > 0)
 	{
-		printf("%d", a);	
+		printf("%d", a);
 	}
 
-    else if (a < 0) 
+	else if (a < 0)
 	{
 		return 0;
 	}
-    //c = a + b;
-    //printf("%d + %d = %d\n", a, b, c);
-    return 0;
+	//c = a + b;
+	//printf("%d + %d = %d\n", a, b, c);
+	return 0;
 }
